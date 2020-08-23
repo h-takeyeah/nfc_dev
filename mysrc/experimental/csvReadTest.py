de420cde090f5a2e499f53ab7e86a4f0a2323a76
h-takeyeah <who26second@gmail.com>
2020-08-23 21:26:22 +0900

    modified .gitignore

--- mysrc/experimental/csvReadTest.py
+++ mysrc/experimental/csvReadTest.py
@@ -3,10 +3,12 @@
 import csv
 
 def main():
-    with open('./non_holder_list.csv') as csvfile:
-        holder = csv.reader(csvfile, delimiter=',') # create a reader object
-        for row in holder:
-            print(row)
+    with open('../memberlist/memberlist_utf8.csv', newline='') as csvfile:
+        members = csv.reader(csvfile, delimiter=',') # create a reader object
+        #print(next(members)) # iterator's __next()__
+        with open('../')
+        for row in members:
+            print(row[0])
 
 if __name__ == '__main__':
     main()
--- mysrc/experimental/csvWriteTest.py
+++ mysrc/experimental/csvWriteTest.py
@@ -5,16 +5,25 @@ import datetime,csv,numParse
 HIST_FILE = str(datetime.datetime.now().year) + '-' + str(datetime.datetime.now().month) # TODO {YY-MM} Is this best?
 
 def main():
-    with open('../history/{}.csv'.format(HIST_FILE), 'a') as outfile: # mode='a': append
-        while True:
-            writer = csv.writer(outfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL) 
-            # memo
-            # quotechar='"': abc -> "abc"
-            # quoting=csv.QUOTE_ALL: a,b,c -> "a","b","c"
-            dt = datetime.datetime.now()
-            num = numParse.main()
-            writer.writerow([num, dt.date(), dt.time().isoformat('seconds')])
-            print("[{}] Completed".format(datetime.datetime.now().isoformat(' ', 'seconds')))
+    with open('../../memberlist/memberlist_utf8.csv', newline='') as source:
+        members = csv.reader(source, delimiter=',') # create a reader object
+        with open('../../history/{}.csv'.format(HIST_FILE), 'a') as outfile: # mode='a': append
+            while True:
+                writer = csv.writer(outfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL) 
+                # memo
+                # quotechar='"': abc -> "abc"
+                # quoting=csv.QUOTE_ALL: a,b,c -> "a","b","c"
+                dt = datetime.datetime.now()
+                num = numParse.main()
+                #for row in members:
+                #    print(type(row))
+                #    print(type(row[0]))
+                #    print(type(row[1]))
+                #    break
+                #    if (num == row[0]):
+                writer.writerow([num, dt.date(), dt.time().isoformat('seconds')])
+                print("[{}] Completed".format(datetime.datetime.now().isoformat(' ', 'seconds')))
+                #        break
 
 if __name__ == '__main__':
     main()
