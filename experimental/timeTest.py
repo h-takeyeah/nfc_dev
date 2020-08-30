5fcd3299aba110a44ec4459ac1d26aaa9920e5e4
h-takeyeah <who26second@gmail.com>
2020-08-30 14:30:30 +0900

    Delete(Untracked) experimental dir; Daily log -> Monthly

--- experimental/timeTest.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import datetime
-today = datetime.datetime.now().date()
-now = datetime.datetime.now().time()
-print(today)
-print(type(today))
-print(now.isoformat('seconds'))
-print(type(now))
