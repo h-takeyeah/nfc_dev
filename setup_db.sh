e9a1851fddc17db757ee72578089341b22c92cb3
h-takeyeah <61489178+h-takeyeah@users.noreply.giyhub.com>
2020-12-07 21:05:56 +0900

    Update acceess_manager.py to use 'set' as member_list. Fixed typo in setup_db.sh

--- setup_db.sh
+++ setup_db.sh
@@ -6,13 +6,13 @@ DATA_TO_INSERT="./memberlist/memberlist_utf8.csv"
 echo "Enter password for root(not unix root)"
 echo -n "Enter password: "
 read ROOTPASS
-echo -e "\nEnter password for nomal user(new)"
+echo -e "\nEnter password for normal(new)"
 echo -n "Enter password: "
-read NOMALPASS
-echo "password for 'nomal' user: ${NOMALPASS}" >> ./log/setup_db_`date "+%F"`.log
+read NORMALPASS
+echo "password for 'normal': ${NORMALPASS}" >> ./log/setup_db_`date "+%F"`.log
 echo "OK"
 mysql -h${HOSTNAME} -P${PORT} -uroot -p${ROOTPASS} --verbose -e "CREATE DATABASE IF NOT EXISTS accessdb"
-mysql -h${HOSTNAME} -P${PORT} -uroot -p${ROOTPASS} --verbose -e "CREATE USER IF NOT EXISTS nomal@'localhost' IDENTIFIED BY '${NOMALPASS}'"
+mysql -h${HOSTNAME} -P${PORT} -uroot -p${ROOTPASS} --verbose -e "CREATE USER IF NOT EXISTS normal@'localhost' IDENTIFIED BY '${NORMALPASS}'"
 mysql -h${HOSTNAME} -P${PORT} -uroot -p${ROOTPASS} --verbose -e "GRANT ALL ON accessdb.* TO nomal@'localhost'"
 mysql -h${HOSTNAME} -P${PORT} -uroot -p${ROOTPASS} --verbose < ./schema/create_table_member_list.sql
 mysql -h${HOSTNAME} -P${PORT} -uroot -p${ROOTPASS} --verbose < ./schema/create_table_access_log.sql
--- src/access_manager.py
+++ src/access_manager.py
@@ -1,7 +1,7 @@
 import nfc
 import mysql.connector as mc
 from auto_close_cursor import AutoCloseCursor as atclscur
-import soundutils
+import soundutils as su
 import dispatcherutils
 
 PASORI_S380_PATH = 'usb:054c:06c3'
@@ -14,7 +14,6 @@ class AccessManager:
 
     Attributes
     ----------
-        get_member : 学生証から読み取った学籍番号をメンバーの名前を対応させる
         cnx : mysql.connector.connection.MySQLConnection
         judge_action : 入室タッチか退室タッチか判定する
 
@@ -22,7 +21,7 @@ class AccessManager:
 
     def __init__(self):
         self.cnx = None
-        self.member_list = {}
+        self.member_list = set()
 
     def connect_to_db(self, config): # config : <class 'dict'>
         """
@@ -50,10 +49,10 @@ class AccessManager:
             return False
         
         with atclscur(self.cnx) as cur:
-            cur.execute('SELECT id,name FROM member_list')
+            cur.execute('SELECT id FROM member_list')
             response = cur.fetchall()
             for row in response:
-                self.member_list[row[0]] = row[1]
+                self.member_list.add(row[0])
 
         return True
 
@@ -83,30 +82,37 @@ class AccessManager:
         """
         try:
             sc = nfc.tag.tt3.ServiceCode(0x200B >> 6, 0x200B & 0x3f)
-            bc = nfc.tag.tt3.BlockCode(0, service=0) # To get student ID
+            bc = nfc.tag.tt3.BlockCode(0, service=0) # To get member ID
             block_data = tag.read_without_encryption([sc], [bc])
 
+        # cf. https://github.com/nfcpy/nfcpy/blob/master/src/nfc/tag/__init__.py
         except nfc.tag.TagCommandError as e:
-            print('\033[01;31m[!] {}\n\033[01;33m[!] Too short. Please touch your card again\033[0m\n'.format(e)) # Text color red
-
-        try:
-            if not self.insert_record(block_data[1:9].decode('utf-8')): # Insert failue
+            su.play_voice('warning')
+            if e.errno == 0x1A6: # 0x01A6 => "invalid service code number or attribute"
+                print('\033[01;33m[!]\033[0m Your IC card seems to be unavailable. Is it valid one?\n')
                 return False
-        except UnboundLocalError: # local variable 'block_data' referenced before assignment
-            print('\033[01;33m[!] Your card seems to be unavailable. \n[!] Is this really an ID card for Shizuoka University?\033[0m\n')
+            elif e.errno == 0x0: # nfc.tag.TIMEOUT_ERROR => "unrecoverable timeout error"
+                print('\033[01;33m[!]\033[0m Too short. Please touch your card again\n') # Text color red
+                return False
+            else:
+                print('\033[01;33m[!]\033[0m {}\n'.format(e))
+                return False
+
+        if not self.insert_record(int(block_data[1:9].decode('utf-8'))): # Insert failue
+            return False
 
         return True
 
-    def insert_record(self, student_id):
+    def insert_record(self, member_id):
         """データベースにINSERT文かUPDATE文を発行して入退室記録をつける
            access_logにクエリを出す
            学籍番号にまつわるレコードのうち入室時間は記録されていて退室時間がNULLであるものを取ってくる
            そのようなレコードがなければ入室のアクションなので新たに挿入する(INSERT)
-           そのようなレコードがあればそのレコードに対してexited_at列に退室時間を追加するという更新処理を施す(UPDATE)
+           そのようなレコードがあればそのレコードのexited_at列を現在時刻で更新する(UPDATE)
 
         Parameters
         ----------
-            student_id : str
+            member_id : int
 
         Returns
         -------
@@ -114,16 +120,20 @@ class AccessManager:
                 INSERT(またはUPDATE)が成功したらTrue,それ以外ならFalse
 
         """
+        if member_id not in self.member_list: # is member?
+            print('\033[01;33m[!]\033[0m Your ID is not registered.\n')
+            su.play_voice('error')
+            return False
+
         with atclscur(self.cnx) as cur:
-            cur.execute('SELECT student_id,entered_at FROM access_log WHERE student_id = {} AND exited_at IS NULL'.format(student_id))
+            cur.execute('SELECT member_id,entered_at FROM access_log WHERE member_id = {} AND exited_at IS NULL'.format(member_id))
             res = cur.fetchone()
 
-        who = self.get_member(student_id)
         action = 'enter'
-        query = 'INSERT INTO access_log (student_id) VALUES ({})'.format(student_id) # enter
+        query = 'INSERT INTO access_log (member_id) VALUES ({})'.format(member_id) # enter
         if res != None:
             action = 'exit'
-            query = 'UPDATE access_log SET exited_at=NOW() WHERE student_id = {} AND entered_at = \'{}\''.format(res[0], res[1]) # exit
+            query = 'UPDATE access_log SET exited_at=NOW() WHERE member_id = {} AND entered_at = \'{}\''.format(res[0], res[1]) # exit
 
         with atclscur(self.cnx) as cur:
             try:
@@ -137,30 +147,9 @@ class AccessManager:
                 print('\033[01;31m[!] Return due to SQL error.\033[0m')
                 return False
 
-            soundutils.play_voice(action) # Greeting
+            su.play_voice(action) # Greeting
 
-            props = {'id': who[0], 'name': who[1], 'action': action}
+            props = {'id': member_id, 'action': action}
             dispatcherutils.dispatch_touch_event(props) # Dispatch information on enttry/exited to View app
             return True
-
-    def get_member(self, whose_id):
-        """学籍番号をキーにして学籍番号と名前のタプルを返す
-           学籍番号が登録されているもの以外だった場合は氏名を'Guest'として返す
-
-        Parameters
-        ----------
-            whose_id : str
-                学籍番号
-
-        Returns
-        -------
-            tuple
-                (学籍番号, 氏名)
-
-        """
-        if int(whose_id) in self.member_list:
-            return (whose_id, self.member_list[int(whose_id)])
-        
-        else:
-            return (whose_id, 'Guest') # Make sure to use 'tuple' (not 'list')
 
